e984fb6a5a731e1946152c31cc7d1ee0a387f09f
zhaopeng <zhaopeng@37.com>
2022-08-22 18:27:05 +0800

    移除：删除sample-base

--- host/sample-host/src/main/java/com/shadow/study/MainActivity.java
+++ host/sample-host/src/main/java/com/shadow/study/MainActivity.java
@@ -1,11 +1,9 @@
 package com.shadow.study;
 
-import static com.tencent.shadow.sample.constant.Constant.PART_KEY_PLUGIN_BASE;
+import static com.tencent.shadow.sample.constant.Constant.PART_KEY_PLUGIN_MAIN_APP;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.ViewGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,9 +12,6 @@ import com.shadow.study.plugin.PluginLoadActivity;
 import com.tencent.shadow.sample.constant.Constant;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ViewGroup mViewGroup;
-    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +22,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-
-        mViewGroup = findViewById(R.id.ll_root);
 
         findViewById(R.id.bt_install_plugin).setOnClickListener(v -> {
             // 安装插件
@@ -59,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
     private void loadPlugin() {
         PluginHelper.getInstance().singlePool.execute(() -> {
             Intent intent = new Intent(MainActivity.this, PluginLoadActivity.class);
-            intent.putExtra(Constant.KEY_PLUGIN_PART_KEY, PART_KEY_PLUGIN_BASE);
+            intent.putExtra(Constant.KEY_PLUGIN_PART_KEY, PART_KEY_PLUGIN_MAIN_APP);
             // 设置插件的启动页
             intent.putExtra(Constant.KEY_ACTIVITY_CLASSNAME, "com.tencent.shadow.sample.plugin.PluginMainActivity");
             startActivity(intent);
